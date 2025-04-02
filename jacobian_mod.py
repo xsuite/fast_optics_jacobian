03dd718c70e952c02ec14441992283b01d94736f
Bernardo Abreu Figueiredo <bernardo.abreufig@outlook.com>
2025-04-02 11:12:33 +0200

    Putting the new Jacobian into the monkey patch, no env yet, so not working

--- jacobian_mod.py
+++ jacobian_mod.py
@@ -10,7 +10,7 @@ def get_dependency_derivatives():
     return all_quad_sources, target_places, dkq_dvv
 
 # collider, opt
-def get_dependency_derivatives(env, opt):
+def get_dependency_derivatives(line, opt):
     class FakeQuad:
         pass
 
@@ -21,14 +21,14 @@ def get_dependency_derivatives(env, opt):
 
         k1 = []
         myelems = {}
-        for dd in env.ref_manager.find_deps([env.vars[vv]]):
+        for dd in line.ref_manager.find_deps([line.vars[vv]]):
             if dd.__class__.__name__ == "AttrRef" and dd._key == "k1":
                 k1.append((dd._owner._key, dd._expr))
                 myelems[dd._owner._key] = FakeQuad()
 
-        fdef = env.ref_manager.mk_fun("myfun", a=env.vars[vv])
+        fdef = line.ref_manager.mk_fun("myfun", a=line.vars[vv])
         gbl = {
-            "vars": env.ref_manager.containers["vars"]._owner.copy(),
+            "vars": line.ref_manager.containers["vars"]._owner.copy(),
             "element_refs": myelems,
         }
         lcl = {}
@@ -47,24 +47,50 @@ def get_dependency_derivatives(env, opt):
     for vv in dkq_dvv.keys():
         all_quad_sources.update(dkq_dvv[vv].keys())
 
+    tt0 = line.get_table()
+    n_points = 10
+    insertions = []
+    points = {}
+    for qqnn in all_quad_sources:
+        assert qqnn in line.element_names
+        qq_start = tt0['s_start', qqnn]
+        qq_end = tt0['s_end', qqnn]
+        s_slice = np.linspace(qq_start, qq_end, n_points)
+        points[qqnn] = []
+        for ii, ss in enumerate(s_slice):
+            nn_point = qqnn + f'_p{ii}'
+            insertions.append(line.new(nn_point, 'marker', at=ss)) ############## env needed!!
+            points[qqnn].append(nn_point)
+    line.insert(insertions)
+
     target_places = set()
     for tt in opt.targets:
         assert isinstance(tt.tar, tuple)
         target_places.add(tt.tar[1])
-    return all_quad_sources, target_places, dkq_dvv
+    return all_quad_sources, target_places, dkq_dvv, points
 ############################################################### here new twiss needed
 
-def get_jac(line, opt, all_quad_sources, target_places, dkq_dvv):
+def get_jac(line, opt, all_quad_sources, target_places, dkq_dvv, points):
     tw0 = line.twiss()
     twiss_derivs = {}
     for qqnn in all_quad_sources:
         twiss_derivs[qqnn] = {}
-        for tt in target_places:
-            twiss_derivs[qqnn][tt] = tw0.get_twiss_param_derivative(src=qqnn, observation=tt)
+    for tt in target_places:
+        twiss_derivs[qqnn][tt] = {}
+        for qqnn_p in points[qqnn]:
+            twiss_derivs[qqnn][tt][qqnn_p] = tw0.get_twiss_param_derivative(src=qqnn_p, observation=tt)
 
-            # Refer to k1 instead of k1l
-            for nn in twiss_derivs[qqnn][tt].keys():
-                twiss_derivs[qqnn][tt][nn] *= line[qqnn].length
+        # Refer to k1 instead of k1l
+            for nn in twiss_derivs[qqnn][tt][qqnn_p].keys():
+                twiss_derivs[qqnn][tt][qqnn_p][nn] *= line[qqnn].length
+
+        # Take the mean of all points preserving the keys
+        mean_values = {}
+        for qqnn_p in points[qqnn]:
+            for nn, val in twiss_derivs[qqnn][tt][qqnn_p].items():
+                mean_values.setdefault(nn, []).append(val)
+
+        twiss_derivs[qqnn][tt] = {nn: np.mean(val) for nn, val in mean_values.items()}
 
     jac_estim = np.zeros((len(opt.targets), len(opt.vary)))
     for itt, tt in enumerate(opt.targets):
@@ -99,8 +125,8 @@ def get_jacobian(self, x, f0=None):
         env_line = self.actions[0].line
         global all_quad_sources, target_places, dkq_dvv
         if all_quad_sources is None or target_places is None or dkq_dvv is None:
-            all_quad_sources, target_places, dkq_dvv = get_dependency_derivatives(env_line, self)
-        jacobian = get_jac(env_line, self, all_quad_sources, target_places, dkq_dvv)
+            all_quad_sources, target_places, dkq_dvv, points = get_dependency_derivatives(env_line, self)
+        jacobian = get_jac(env_line, self, all_quad_sources, target_places, dkq_dvv, points)
         return jacobian
     else:
         if hasattr(self, "_force_jacobian"):
